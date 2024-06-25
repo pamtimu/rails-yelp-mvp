56213029e1dd5b9cd932f04ae349dfdcccefc90f
pamera timu <pamerapaasi@gmail.com>
2024-06-25 23:08:02 +0200

    done but needs styling

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -45,6 +45,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
